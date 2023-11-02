3161d0c44c10a56aacb079cc9aaeb696c812a75b
lazg <lazg@mm-software.com>
2023-11-02 13:08:25 +0800

    feat: build andriod apk

---
+++ src/app/tst-ionic-extension1/tst-ionic-extension1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tst-ionic-extension1',
+  templateUrl: './tst-ionic-extension1.component.html',
+  styleUrls: ['./tst-ionic-extension1.component.scss'],
+})
+export class TstIonicExtension1Component  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
